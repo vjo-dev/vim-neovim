bf6532df6c52d01ce24460567b1687591fcc78d9
Vincent Journot <journot.vincent@gmail.com>
2022-10-30 23:08:29 +0100

    add message for failed init

--- lua/vincent/plugins/autopairs.lua
+++ lua/vincent/plugins/autopairs.lua
@@ -18,12 +18,14 @@ autopairs.setup({
 -- import nvim-autopairs completion functionality safely
 local cmp_autopairs_setup, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
 if not cmp_autopairs_setup then
+  print("nvim-autopairs.completion.cmp not found!")
 	return
 end
 
 -- import nvim-cmp plugin safely (completions plugin)
 local cmp_setup, cmp = pcall(require, "cmp")
 if not cmp_setup then
+  print("cmp not found!")
 	return
 end
 
