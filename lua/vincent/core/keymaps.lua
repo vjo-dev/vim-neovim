4ef0bb23a97c4897ded564c1ad73850810c0d37d
Vincent Journot <journot.vincent@gmail.com>
2022-11-02 20:59:12 +0100

    add keymap to format files instead of formatting on save

--- lua/vincent/core/keymaps.lua
+++ lua/vincent/core/keymaps.lua
@@ -62,3 +62,6 @@ keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current c
 
 -- restart lsp server (not on youtube nvim video)
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+
+-- formatting
+keymap.set("n", "<leader>lf", ":lua vim.lsp.buf.formatting()<CR>")
